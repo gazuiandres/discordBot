5751ebaa310615a8654856197c8d1f3bec8fe876
Andres Gazui <gazuiandres@gmail.com>
2024-05-15 16:16:34 -0400

    testing bot

--- cronjob/tasks/serverReset.js
+++ cronjob/tasks/serverReset.js
@@ -3,31 +3,25 @@ const { GUILD_ID } = process.env;
 const resetServer = (client) => {
   if (!client) return;
 
-  // exec('pm2 stop server', async (err, stdout, stderr) => {
-  //     if(err) {
-  //         console.log('err reseteando el servidor ', err)
-  //         return;
-  //     }
-  //     console.log('Apagando servidor para reseteo')
-  // })
-  // setTimeout(() => {
-  //     console.log('Reiniciando servidor')
-  //     exec('pm2 start server', async (err, stdout, stderr) => {
-  //         if(err) {
-  //             console.log('err reseteando el servidor ', err)
-  //             return;
-  //         }
-  //         console.log('Servidor reiniciado')
-  //     })
-  // }, 30000);
-
   const guild = client.guilds.cache.find((guild) => guild.id === GUILD_ID);
   const channel = guild.channels.cache.find(
     (channel) => channel.name === "minecraft-server"
   );
 
   if (!channel) return;
-  channel.send("probando mensajes con el bot, no se asusten 🥸");
+  channel.send("@Minecraft probando bot");
+  setTimeout(() => {
+    //   console.log('Reiniciando servidor')
+    //   exec('pm2 restart server', async (err, stdout, stderr) => {
+    //       if(err) {
+    //           console.log('err reseteando el servidor ', err)
+    //           return;
+    //       }
+    //       console.log('Servidor reiniciado')
+    //   })
+    channel.send("simulando reseteo automatico, no se asusten");
+  }, 30000);
+  
 };
 
 module.exports = resetServer;
