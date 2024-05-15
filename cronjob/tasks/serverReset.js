684c9b1666a2a66ff561b4ff2e82545b65a7cbbb
Andres Gazui <gazuiandres@gmail.com>
2024-05-15 16:07:22 -0400

    preparing bot

--- cronjob/tasks/serverReset.js
+++ cronjob/tasks/serverReset.js
@@ -1,24 +1,33 @@
-const {exec} = require('child_process')
-const { GUILD_ID } = process.env
+const { exec } = require("child_process");
+const { GUILD_ID } = process.env;
 const resetServer = (client) => {
-    // exec('pm2 stop server', async (err, stdout, stderr) => {
-    //     if(err) {
-    //         console.log('err reseteando el servidor ', err)
-    //         return;
-    //     }
-    //     console.log('Apagando servidor para reseteo')
-    // })
-    // setTimeout(() => {
-    //     console.log('Reiniciando servidor')
-    //     exec('pm2 start server', async (err, stdout, stderr) => {
-    //         if(err) {
-    //             console.log('err reseteando el servidor ', err)
-    //             return;
-    //         }
-    //         console.log('Servidor reiniciado')
-    //     })
-    // }, 30000);
-    console.log(client.guilds.cache.find(guild => guild.id === GUILD_ID))
-}
+  if (!client) return;
 
-module.exports = resetServer
+  // exec('pm2 stop server', async (err, stdout, stderr) => {
+  //     if(err) {
+  //         console.log('err reseteando el servidor ', err)
+  //         return;
+  //     }
+  //     console.log('Apagando servidor para reseteo')
+  // })
+  // setTimeout(() => {
+  //     console.log('Reiniciando servidor')
+  //     exec('pm2 start server', async (err, stdout, stderr) => {
+  //         if(err) {
+  //             console.log('err reseteando el servidor ', err)
+  //             return;
+  //         }
+  //         console.log('Servidor reiniciado')
+  //     })
+  // }, 30000);
+
+  const guild = client.guilds.cache.find((guild) => guild.id === GUILD_ID);
+  const channel = guild.channels.cache.find(
+    (channel) => channel.name === "minecraft-server"
+  );
+
+  if (channel) return;
+  channel.send("probando mensajes con el bot, no se asusten 🥸");
+};
+
+module.exports = resetServer;
