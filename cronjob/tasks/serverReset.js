52749d25e2462329196c2791a9b09b1d898e674c
Andres Gazui <gazuiandres@gmail.com>
2024-11-07 21:17:42 -0500

    refactor:

--- cronjob/tasks/serverReset.js
+++ cronjob/tasks/serverReset.js
@@ -9,9 +9,9 @@ const resetServer = (client) => {
   );
 
   if (!channel) return;
-  channel.send(
-    `<@&1240396602624049344> El servidor se reiniciara por mantenimiento en 5 minutos, tomen sus precauciones 🙆‍♂️`
-  );
+  // channel.send(
+  //   `<@&1240396602624049344> El servidor se reiniciara por mantenimiento en 5 minutos, tomen sus precauciones 🙆‍♂️`
+  // );
   setTimeout(() => {
     console.log("Reiniciando servidor");
     exec("pm2 restart server", async (err, stdout, stderr) => {
@@ -21,7 +21,7 @@ const resetServer = (client) => {
       }
       console.log("Servidor reiniciado");
     });
-    channel.send("<@&1240396602624049344> Servidor inciando nuevamente");
+    // channel.send("<@&1240396602624049344> Servidor inciando nuevamente");
   }, 300000);
 };
 
