4377f37b95c643c1f812a917aa81bb8fdc51b9b8
Andres Gazui <gazuiandres@gmail.com>
2024-11-27 20:12:20 -0500

    fix: :bug: Fix querySelector finder

--- scrappers/twitchViewers.js
+++ scrappers/twitchViewers.js
@@ -9,11 +9,11 @@ const getTwitchViwers = async () => {
 
     const page = await browser.newPage();
 
-    await page.goto("https://www.twitch.tv/alehahze");
+    await page.goto("https://www.twitch.tv/mioshi");
     await page.waitForLoadState('domcontentloaded')
     await page.click('[data-test-selector="chat-viewer-list"]');
     await page.waitForTimeout(4000)
-    const viewersElements = page.locator('.ScCoreLink-sc-16kq0mq-0.esnIuC');
+    const viewersElements = page.locator('.ScCoreLink-sc-16kq0mq-0.hmHcbv.InjectLayout-sc-1i43xsx-0.iJYmpw.tw-link');
     const viewers = await viewersElements.evaluateAll(
         list => list.map(element => element.textContent));
     browser.close()
